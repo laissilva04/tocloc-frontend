1cae8a8a61664a2c98a2b4e82e53a3e74c08de32
Lais Barbosa <laissilva@edu.unifor.br>
2024-11-28 15:22:12 -0300

    implementação do botão de logout

--- src/pages/PlayerHome/PlayerHome.js
+++ src/pages/PlayerHome/PlayerHome.js
@@ -1,15 +1,22 @@
 import React from "react";
-import Sports from "../../components/Sports/Sports";  // Certifique-se de que Sports está sendo importado corretamente
+import Sports from "../../components/Sports/Sports";
+import logout from "../../assets/icons/logout.svg";
+import { Link } from "react-router-dom";
 
 function PlayerHome() {
     return (
         <div className="bg-gray-50">
-            {/* Header Simples e Preto */}
-            <header className="bg-black text-white py-4">
+            
+            <header className="bg-black text-white py-4 flex justify-center">
+                <div className="w-[95%] flex justify-center">
                 <h1 className="text-center font-1-xl mb-3">Tocloc. <span class="cor-p1">Tocou, locou, jogou!</span> </h1>
+                </div>
+                <Link to="/">
+                <button className="mt-4"> <img src={logout} alt="" /></button>
+                </Link>
             </header>
 
-            {/* Section com conteúdo do "Bem-vindo ao Tocloc!" */}
+
             <section className="p-5">
                 <div className="text-center mb-2">
                     <h2 className="text-4xl font-bold text-green-700 mb-2 mt-5 font-1-xl">Bem-vindo!</h2>
