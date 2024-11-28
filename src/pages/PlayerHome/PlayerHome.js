49f2d892eb84c5009e5fbf8aa13c87555b5aee32
Lais Barbosa <laissilva@edu.unifor.br>
2024-11-28 09:14:34 -0300

    add estilo de fonte

--- src/pages/PlayerHome/PlayerHome.js
+++ src/pages/PlayerHome/PlayerHome.js
@@ -6,13 +6,13 @@ function PlayerHome() {
         <div className="bg-gray-50">
             {/* Header Simples e Preto */}
             <header className="bg-black text-white py-4">
-                <h1 className="text-center text-3xl font-bold">Tocloc</h1>
+                <h1 className="text-center font-1-xl">Tocloc</h1>
             </header>
 
             {/* Section com conteúdo do "Bem-vindo ao Tocloc!" */}
             <section className="p-5">
                 <div className="text-center mb-2">
-                    <h2 className="text-4xl font-bold text-green-700 mb-2 mt-10 font-1-xl">Bem-vindo ao Tocloc!</h2>
+                    <h2 className="text-4xl font-bold text-green-700 mb-2 mt-5 font-1-xl">Bem-vindo ao Tocloc!</h2>
                     <p className="text-gray-600 font-2-l-b">Conecte-se com os melhores espaços esportivos e faça sua reserva.</p>
                 </div>
                 <Sports />
